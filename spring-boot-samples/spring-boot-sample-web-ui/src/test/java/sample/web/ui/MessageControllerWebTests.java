8e5ff275352512c8cbb6a679801cbafe8980bde5
anilbidari <anil.bidari@thecloudenabled.com>
2016-12-29 22:30:02 +0530

    Update MessageControllerWebTests.java

--- spring-boot-samples/spring-boot-sample-web-ui/src/test/java/sample/web/ui/MessageControllerWebTests.java
+++ spring-boot-samples/spring-boot-sample-web-ui/src/test/java/sample/web/ui/MessageControllerWebTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//This is new line number 01
 package sample.web.ui;
 
 import java.util.regex.Pattern;
